96a08689b2e55c396fcd2c87c46004e50c09b28d
mrjohnlowlevel <johnfree751@gmail.com>
2025-11-01 12:27:00 +0800

    changed input to able to hold instead of a single press

--- scripts/vin.gd
+++ scripts/vin.gd
@@ -1,14 +1,14 @@
 extends CharacterBody2D
 
 func _process(_delta: float) -> void:
-    if Input.is_action_just_pressed("move_left"):
+    if Input.is_action_pressed("move_left"):
         print("Test: left")
     
-    if Input.is_action_just_pressed("move_right"):
+    if Input.is_action_pressed("move_right"):
         print("Test: right")
     
-    if Input.is_action_just_pressed("move_up"):
+    if Input.is_action_pressed("move_up"):
         print("Test: Up")
     
-    if Input.is_action_just_pressed("move_down"):
+    if Input.is_action_pressed("move_down"):
         print("Test: down")
