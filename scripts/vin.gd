d03e37647b63eaef6ba6611c0d9bc08592c2cb05
mrjohnlowlevel <johnfree751@gmail.com>
2025-11-17 07:15:49 +0800

    added meaningful comments

--- scripts/vin.gd
+++ scripts/vin.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
-@onready var _animated_sprite = $AnimatedSprite2D
+@onready var vin_sprite = $AnimatedSprite2D
 
 var speed = 125
 
 func _ready() -> void:
-	_animated_sprite.play("idle_down")
+	vin_sprite.play("idle_down")
 
 func get_input() -> void:
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -13,27 +13,36 @@ func get_input() -> void:
 
 func facing_direction() -> void:
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+
 	if direction == Vector2(1,0):
-		_animated_sprite.play("walking_right")
+		vin_sprite.play("walking_right")
+
 	elif direction == Vector2(-1,0):
-		_animated_sprite.play("walking_left")
+		vin_sprite.play("walking_left")
+
 	elif direction == Vector2(0,1):
-		_animated_sprite.play("walking_down")
+		vin_sprite.play("walking_down")
+
 	elif  direction == Vector2(0,-1):
-		_animated_sprite.play("walking_up")
+		vin_sprite.play("walking_up")
+
+	# Diagonal check
 	elif direction.y > 0:
-		_animated_sprite.play("walking_down")
+		vin_sprite.play("walking_down")
+
 	elif direction.y < 0:
-		_animated_sprite.play("walking_up")
+		vin_sprite.play("walking_up")
+
 	else:
-		if _animated_sprite.animation == "walking_right":
-			_animated_sprite.play("idle_right")
-		elif _animated_sprite.animation == "walking_left":
-			_animated_sprite.play("idle_left")
-		elif _animated_sprite.animation == "walking_up":
-			_animated_sprite.play("idle_up")
-		elif _animated_sprite.animation == "walking_down":
-			_animated_sprite.play("idle_down")
+		# Idle anim block check
+		if vin_sprite.animation == "walking_right":
+			vin_sprite.play("idle_right")
+		elif vin_sprite.animation == "walking_left":
+			vin_sprite.play("idle_left")
+		elif vin_sprite.animation == "walking_up":
+			vin_sprite.play("idle_up")
+		elif vin_sprite.animation == "walking_down":
+			vin_sprite.play("idle_down")
 
 func _physics_process(_delta) -> void:
 	get_input()
