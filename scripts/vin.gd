3d840bc16d2e6b51cddc78413f83d0eab9fb12fd
mrjohnlowlevel <johnfree751@gmail.com>
2025-11-25 17:27:32 +0800

    simplified some more code. TODO: Fix diagonal issue

--- scripts/vin.gd
+++ scripts/vin.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 # !GLOBAL_VAR_ECTION
 
 @onready var vin_sprite = $AnimatedSprite2D
+@onready var dbg_lbl = $Label
 var speed = 125
 
 # !END_GLOBAL_VAR
@@ -13,30 +14,31 @@ func _ready() -> void:
 func sprite_play(sprite: AnimatedSprite2D, animation: String) -> void:
 	sprite.play(animation)
 
+func play_idle(sprite: AnimatedSprite2D) -> void:
+	match vin_sprite.animation:
+		"walking_right": sprite_play(sprite, "idle_right")
+		"walking_left": sprite_play(sprite, "idle_left")
+		"walking_down": sprite_play(sprite, "idle_down")
+		"walking_up": sprite_play(sprite, "idle_up")
+
 func get_movemnts_inp() -> void:
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
 	if Input.is_action_pressed("sprint_btn"):
 		self.velocity = direction * (speed * 2)
 	else:
 		self.velocity = direction * speed
 
 func facing_direction() -> void:
-	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down").normalized()
+	dbg_lbl.text = "Vec2(%d,%d)" % [direction.x, direction.y]
 
 	match direction:
-		Vector2(1,0): sprite_play(vin_sprite, "walking_right")
-		Vector2(-1,0): sprite_play(vin_sprite, "walking_left")
-		Vector2(0,1): sprite_play(vin_sprite, "walking_down")
-		Vector2(0,-1): sprite_play(vin_sprite, "walking_up")
+		Vector2(1, 0): sprite_play(vin_sprite, "walking_right")
+		Vector2(-1, 0): sprite_play(vin_sprite, "walking_left")
+		Vector2(0, 1): sprite_play(vin_sprite, "walking_down")
+		Vector2(0, -1): sprite_play(vin_sprite, "walking_up")
 		_:
-			match vin_sprite.animation:
-				"walking_right": sprite_play(vin_sprite, "idle_right")
-				"walking_left": sprite_play(vin_sprite, "idle_left")
-				"walking_down": sprite_play(vin_sprite, "idle_down")
-				"walking_up": sprite_play(vin_sprite, "idle_up")
-
-	if direction.y < 0: sprite_play(vin_sprite, "walking_up")
-	if direction.y > 0: sprite_play(vin_sprite, "walking_down")
+			play_idle(vin_sprite)
 
 func _physics_process(_delta) -> void:
 	get_movemnts_inp()
