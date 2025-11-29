a62b0d1ea908440fea5e8ecca5fa418f2d66491c
Loonieth <akiffirasrashid@gmail.com>
2025-11-29 18:03:52 +0800

    added sprinting animation

--- scripts/vin.gd
+++ scripts/vin.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 @onready var vin_sprite = $AnimatedSprite2D
 var speed = 125
-
+var sprint = false
 # !END_GLOBAL_VAR
 
 func _ready() -> void:
@@ -15,23 +15,31 @@ func sprite_play(sprite: AnimatedSprite2D, animation: String) -> void:
 
 func play_idle(sprite: AnimatedSprite2D) -> void:
 	match vin_sprite.animation:
-		"walking_right": sprite_play(sprite, "idle_right")
-		"walking_left": sprite_play(sprite, "idle_left")
-		"walking_down": sprite_play(sprite, "idle_down")
-		"walking_up": sprite_play(sprite, "idle_up")
+		"walking_right", "sprint_right": sprite_play(sprite, "idle_right")
+		"walking_left", "sprint_left": sprite_play(sprite, "idle_left")
+		"walking_down", "sprint_down": sprite_play(sprite, "idle_down")
+		"walking_up", "sprint_up": sprite_play(sprite, "idle_up")
 
 func get_movemnts_inp(usr_direction: Vector2) -> void:
 	if Input.is_action_pressed("sprint_btn"):
 		self.velocity = usr_direction * (speed * 1.5)
+		sprint = true
 	else:
 		self.velocity = usr_direction * speed
+		sprint = false
 
 func facing_direction(usr_direction: Vector2) -> void:
 	if usr_direction != Vector2.ZERO:
 		if abs(usr_direction.x) > abs(usr_direction.y):
-			sprite_play(vin_sprite, "walking_right" if usr_direction.x > 0 else "walking_left")
+			if sprint:
+				sprite_play(vin_sprite, "sprint_right" if usr_direction.x > 0 else "sprint_left")
+			else:
+				sprite_play(vin_sprite, "walking_right" if usr_direction.x > 0 else "walking_left")
 		else:
-			sprite_play(vin_sprite, "walking_down" if usr_direction.y > 0 else "walking_up")
+			if sprint:
+				sprite_play(vin_sprite, "sprint_down" if usr_direction.y > 0 else "sprint_up")
+			else:
+				sprite_play(vin_sprite, "walking_down" if usr_direction.y > 0 else "walking_up")
 	else:
 		play_idle(vin_sprite)
 
