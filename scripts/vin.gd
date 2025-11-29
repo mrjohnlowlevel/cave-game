8fe7605c99971700ca9bf97f63d60b7d853c79fa
mrjohnlowlevel <johnfree751@gmail.com>
2025-11-29 18:37:56 +0800

    changed is_sprint's (formerly sprint) state to be local instead of global

--- scripts/vin.gd
+++ scripts/vin.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 @onready var vin_sprite = $AnimatedSprite2D
 var speed = 125
-var sprint = false
+
 # !END_GLOBAL_VAR
 
 func _ready() -> void:
@@ -20,23 +20,23 @@ func play_idle(sprite: AnimatedSprite2D) -> void:
 		"walking_down", "sprint_down": sprite_play(sprite, "idle_down")
 		"walking_up", "sprint_up": sprite_play(sprite, "idle_up")
 
-func get_movemnts_inp(usr_direction: Vector2) -> void:
+func get_movemnts_inp(usr_direction: Vector2) -> bool:
 	if Input.is_action_pressed("sprint_btn"):
 		self.velocity = usr_direction * (speed * 1.5)
-		sprint = true
+		return true
 	else:
 		self.velocity = usr_direction * speed
-		sprint = false
+		return false
 
-func facing_direction(usr_direction: Vector2) -> void:
+func facing_direction(usr_direction: Vector2, is_sprint: bool) -> void:
 	if usr_direction != Vector2.ZERO:
 		if abs(usr_direction.x) > abs(usr_direction.y):
-			if sprint:
+			if is_sprint:
 				sprite_play(vin_sprite, "sprint_right" if usr_direction.x > 0 else "sprint_left")
 			else:
 				sprite_play(vin_sprite, "walking_right" if usr_direction.x > 0 else "walking_left")
 		else:
-			if sprint:
+			if is_sprint:
 				sprite_play(vin_sprite, "sprint_down" if usr_direction.y > 0 else "sprint_up")
 			else:
 				sprite_play(vin_sprite, "walking_down" if usr_direction.y > 0 else "walking_up")
@@ -44,7 +44,9 @@ func facing_direction(usr_direction: Vector2) -> void:
 		play_idle(vin_sprite)
 
 func _physics_process(_delta) -> void:
+	var is_sprint = false
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	get_movemnts_inp(direction)
-	facing_direction(direction)
+
+	is_sprint = get_movemnts_inp(direction)
+	facing_direction(direction, is_sprint)
 	move_and_slide()
